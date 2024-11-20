3d3099e934c7f5f9a2c06c43eec00b5322af2387
SruthiPrasannakumar <sruthipras93@gmail.com>
2024-11-20 10:03:46 +0530

    cart completed

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -3,8 +3,10 @@ import Header from '../components/Header'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToWishlist } from '../redux/slices/wishlistSlice'
+import { addToCart } from '../redux/slices/cartSlice'
 
 const View = () => {
+  const userCart=useSelector(state=>state.cartReducer)
   const  dispatch=useDispatch()
   const userWishlist=useSelector(state=>state.wishlistReducer)
   const [product,setProduct]=useState({})
@@ -30,16 +32,26 @@ const View = () => {
     }
   }
   
+  const handleCart=()=>{
+    dispatch(addToCart(product))
+    const existingProduct=userCart?.find(item=>item?.id==id)
+    if(existingProduct){
+      alert("Product quantity is incrementing in your cart!!")
+    }else{
+      alert("Product added to your cart")
+    }
+  }
+  
   
   return (
     <>
     <Header/>
-    <div className='flex flex-col mx-5'>
-        <div className="grid grid-cols-2 items-center h-screen">
+    <div className='flex flex-col mx-5 pt-11 '>
+        <div className="grid grid-cols-2 items-center h-screen  ">
             <img className='ms-40' width={'350px'} height={'250px'} src={product?.thumbnail} alt="" />
         
-       <div>
-            <h3 className="font-bold">PID : {product?.id}</h3>
+       <div >
+            <h3 className="font-bold ">PID : {product?.id}</h3>
             <h1 className="text-5xl font-bold">{product?.title}</h1>
             <h4 className="font-bold text-red-600 text-2xl">${product?.price}</h4>
             <h4>Brand:{product?.brand}</h4>
@@ -70,7 +82,7 @@ const View = () => {
 
             <div className="flex justify-between mt-5">
               <button onClick={handleWishlist} className="bg-blue-600 rounded text-white p-2">ADD TO WISHLIST</button>
-              <button className="bg-green-600 rounded text-white p-2">ADD TO CART</button>
+              <button onClick={handleCart} className="bg-green-600 rounded text-white p-2">ADD TO CART</button>
             </div>
        </div>
     </div>
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import Header from '../components/Header'
 import { useDispatch, useSelector } from 'react-redux'
 import { removeItem } from '../redux/slices/wishlistSlice'
+import { addToCart } from '../redux/slices/cartSlice'
+
 
 const Wishlist = () => {
+  const userCart=useSelector(state=>state.cartReducer)
   const dispatch=useDispatch()
   const userWishlist=useSelector(state=>state.wishlistReducer)
+
+  const handleCart=(product)=>{
+    dispatch(removeItem(product.id))
+    dispatch(addToCart(product))
+    const existingProduct=userCart?.find(item=>item?.id==product.id)
+    if(existingProduct){
+      alert("Product quantity is incrementing in your cart!!")
+    }else{
+      alert("Product added to your cart")
+    }
+  }
   return (
     <>
     <Header/>
@@ -24,7 +38,7 @@ const Wishlist = () => {
                 <h3 className='text-xl font-bold'>{product?.title}</h3>
                <div className='flex justify-evenly mt-3'>
                 <button onClick={()=>dispatch(removeItem(product?.id))} className='text-xl'><i className='fa-solid fa-heart-circle-xmark text-red-500'></i></button>
-                <button className='text-xl'><i className='fa-solid fa-cart-plus text-green-700'></i></button>
+                <button onClick={()=>handleCart(product)} className='text-xl'><i className='fa-solid fa-cart-plus text-green-700'></i></button>
 
                </div>
             </div>
@@ -35,7 +49,7 @@ const Wishlist = () => {
         </>
         :
         <div className="flex flex-col justify-center items-center">
-          <img className='w-100 h-1/2' src="" alt="" />
+          <img className='w-100 h-1/2' src="https://www.adanione.com/~/media/Foundation/Adani/emptyImages/empty_cart.gif" alt="" />
           <h1 className='text-4xl text-red-600 mt-3'>Your wishlist is empty!!</h1>
         </div>
       }
